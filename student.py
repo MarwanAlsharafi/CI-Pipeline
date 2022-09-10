57fdf5eda98d5ba432a678598108661b45108e3c
Marwan Alsharafi <60095071@udst.edu.qa>
2022-09-10 21:38:14 +0300

    Add test objects

--- student.py
+++ student.py
@@ -1,5 +1,9 @@
+from unittest import TestCase
+
+
 class student:
     def __init__(self,name,age,id):
+
         if type(name)!=str:
             raise TypeError("Name must be string")
         if type(age) != int:
@@ -27,7 +31,8 @@ class student:
         def age(self):
             return self._age
 
-from unittest import TestCase
+        
+
 
 
 class Student_Test(TestCase):
@@ -47,7 +52,7 @@ class Student_Test(TestCase):
 
     
     def test_age(self):
-        testStudent = student("Marwan", 22, 60095071)
+        testStudent = student("Marwan", -22, 60095071)
 
         self.assertEqual(testStudent.age, 22)
 
@@ -55,12 +60,12 @@ class Student_Test(TestCase):
         with self.assertRaises(TypeError):
             student("Marwan", -1, 1)
 
-#to check if the methods works
+#to test the student info 
 
 s=Student_Test()
 #s.test_name()
 #s.test_id()
-#s.test_age()
+s.test_age()
 #s.test_if_Age_is_negative()
 #s.test_If_ID_Is_negative()
 
